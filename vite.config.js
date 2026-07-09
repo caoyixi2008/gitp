f8e51b30113de1c9d0baadbc5e53ab4ccb6fc85d
zhuangjinhui <2646002117@qq.com>
2026-07-09 14:52:01 +0800

    配置双后端代理

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,31 @@ export default defineConfig({
 
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+
+      // ===== 运动 =====
+      '/api/sport': {
+        target: 'http://10.133.132.79:8080',
+        changeOrigin: true
+      },
+
+      // ===== 饮食 =====
+      '/api/diet': {
+        target: 'http://10.133.132.79:8080',
+        changeOrigin: true
+      },
+
+      // ===== 健康提醒 =====
+      '/api/notice': {
+        target: 'http://10.133.132.79:8080',
+        changeOrigin: true
+      },
+
+      // ===== 体重 =====
+      '/api/user/weight': {
+        target: 'http://10.133.134.227:8081',
         changeOrigin: true
       }
+
     }
   }
 })
